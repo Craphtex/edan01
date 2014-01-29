1edb0f20e363ff9a9139f495d88257624264a6c7
Craphtex <christian@crapht.se>
2014-01-29 15:11:35 +0100

    Some formatting

--- src/lab1/GateModel.java
+++ src/lab1/GateModel.java
@@ -1,9 +1,6 @@
 package lab1;
 
 import org.jacop.constraints.AndBool;
-import org.jacop.constraints.Not;
-import org.jacop.constraints.OrBool;
-import org.jacop.constraints.XeqY;
 import org.jacop.core.IntVar;
 import org.jacop.core.Store;
 import org.jacop.search.DepthFirstSearch;
@@ -18,15 +15,15 @@ public class GateModel {
         // Something to start from
         Store store = new Store(); // define FD store
         // define finite domain variables
-        IntVar a = new IntVar(store,"a",0,1);
-        IntVar b = new IntVar(store,"b",0,1);
-        IntVar c = new IntVar(store,"c",0,1);
-        IntVar ab = new IntVar(store,"ab",0,1);
-        IntVar s = new IntVar(store,"s",0,1);
-        IntVar[] v = new IntVar[]{a,b,c, ab, s};
+        IntVar a = new IntVar(store, "a", 0, 1);
+        IntVar b = new IntVar(store, "b", 0, 1);
+        IntVar c = new IntVar(store, "c", 0, 1);
+        IntVar ab = new IntVar(store, "ab", 0, 1);
+        IntVar s = new IntVar(store, "s", 0, 1);
+        IntVar[] v = new IntVar[]{a, b, c, ab, s};
 
-        store.impose(new AndBool(new IntVar[]{a,b}, ab));
-        store.impose(new AndBool(new IntVar[]{ab,c},s));
+        store.impose(new AndBool(new IntVar[]{a, b}, ab));
+        store.impose(new AndBool(new IntVar[]{ab, c}, s));
 
 
         // search for a solution and print results
@@ -41,9 +38,9 @@ public class GateModel {
 
         if (result) {
             System.out.println("a  b  c  ab s");
-            for (int i=1; i<=search.getSolutionListener().solutionsNo(); i++){
-                for (int j=0; j<search.getSolution(i).length; j++)
-                    System.out.print(search.getSolution(i)[j]+"  ");
+            for (int i = 1; i <= search.getSolutionListener().solutionsNo(); i++) {
+                for (int j = 0; j < search.getSolution(i).length; j++)
+                    System.out.print(search.getSolution(i)[j] + "  ");
                 System.out.println();
             }
 
