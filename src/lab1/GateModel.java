238642f902e7e825a0ed288ac78962d741dde945
ada10lgu <ada10lgu@student.lth.se>
2014-01-29 14:39:20 +0100

    Changed to a&b&c

--- src/lab1/GateModel.java
+++ src/lab1/GateModel.java
@@ -1,13 +1,17 @@
 package lab1;
 
-import org.jacop.constraints.And;
 import org.jacop.constraints.AndBool;
+import org.jacop.constraints.Not;
+import org.jacop.constraints.OrBool;
 import org.jacop.constraints.XeqY;
-import org.jacop.constraints.XneqY;
-import org.jacop.core.BooleanVar;
 import org.jacop.core.IntVar;
 import org.jacop.core.Store;
-import org.jacop.search.*;
+import org.jacop.search.DepthFirstSearch;
+import org.jacop.search.IndomainMin;
+import org.jacop.search.InputOrderSelect;
+import org.jacop.search.PrintOutListener;
+import org.jacop.search.Search;
+import org.jacop.search.SelectChoicePoint;
 
 public class GateModel {
     public static void main(String[] args) {
@@ -16,10 +20,16 @@ public class GateModel {
         // define finite domain variables
         IntVar a = new IntVar(store,"a",0,1);
         IntVar b = new IntVar(store,"b",0,1);
+        IntVar c = new IntVar(store,"c",0,1);
+        IntVar ab = new IntVar(store,"ab",0,1);
         IntVar s = new IntVar(store,"s",0,1);
-        IntVar[] v = new IntVar[]{a,b};
+        IntVar[] v = new IntVar[]{a,b,c};
 
-        store.impose(new AndBool(v,s));
+        IntVar[] andAB = new IntVar[]{a,b};
+        IntVar[] andABC = new IntVar[]{ab,c};
+        
+        store.impose(new AndBool(andAB, ab));
+        store.impose(new AndBool(andABC,s));
 
         // search for a solution and print results
         Search<IntVar> search = new DepthFirstSearch<IntVar>();
@@ -30,7 +40,7 @@ public class GateModel {
         
         if (result) {
             System.out.println("Solution:");
-            System.out.printf("a:%d b:%d c:%d", a.value(), b.value(), s.value());
+            System.out.printf("a:%d b:%d c:%d   a&b&c=%d", a.value(), b.value(),c.value(), s.value());
         } else System.out.println("No solution");
     }
 }
