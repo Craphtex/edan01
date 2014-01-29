930e0e71a745dfa79fe58422d1e0c2a166a27b00
ada10lgu <ada10lgu@student.lth.se>
2014-01-29 16:03:32 +0100

    Finished Ex3
    No solution found if a->i is different.
    Is this a fault??

--- src/lab1/Chandelier.java
+++ src/lab1/Chandelier.java
@@ -30,18 +30,44 @@ public class Chandelier {
         IntVar h = new IntVar(store,"h",1,9);
         IntVar i = new IntVar(store,"i",1,9);
         
-        IntVar[] v = new IntVar[]{a,b,c,d,e,f,g,h,i};
         
-        IntVar[] abc = new IntVar[]{a,b,c};
-        IntVar[] def = new IntVar[]{d,e,f};
-        IntVar[] ghi = new IntVar[]{g,h,i};
-        
-        IntVar bajs = new IntVar(store,"bajs",1,9);
-        new Sum(abc,bajs);
+        //store.impose(new Alldiff(new IntVar[]{a,b,c,d,e,f,g,h,i}));
+
+        IntVar x = new IntVar(store,"x",1,100);
+        IntVar y = new IntVar(store,"y",1,100);
+        IntVar z = new IntVar(store,"z",1,100);
         
         
-        store.impose(new Alldiff(v));
-        store.impose(new XeqY(bajs,d));
+        store.impose(new Sum(new IntVar[]{a,b,c},y));
+        store.impose(new Sum(new IntVar[]{d,e,f},z));
+        store.impose(new Sum(new IntVar[]{g,h,i},x));
+        
+        IntVar yy = new IntVar(store,"yy",0,100);
+        store.impose(new Sum(new IntVar[]{y,y,y},yy));
+        IntVar xz = new IntVar(store,"xz",0,100);
+        store.impose(new Sum(new IntVar[]{z,z,x,x,x},xz));
+        store.impose(new XeqY(yy,xz));
+        
+        IntVar bc = new IntVar(store,"bc",0,100);
+        store.impose(new Sum(new IntVar[]{b,c,c},bc));
+        IntVar aa = new IntVar(store,"aa",0,100);
+        store.impose(new Sum(new IntVar[]{a,a},aa));
+        store.impose(new XeqY(bc,aa));
+                
+        IntVar de = new IntVar(store,"de",0,100);
+        store.impose(new Sum(new IntVar[]{d,d,e},de));
+        IntVar ff = new IntVar(store,"ff",0,100);
+        store.impose(new Sum(new IntVar[]{f,f},ff));
+        store.impose(new XeqY(de,ff));
+        
+        IntVar gh = new IntVar(store,"gh",0,100);
+        store.impose(new Sum(new IntVar[]{g,g,h},gh));
+        IntVar ii = new IntVar(store,"ii",0,100);
+        store.impose(new Sum(new IntVar[]{i,i,i},ii));
+        store.impose(new XeqY(gh,ii));
+        
+        
+        IntVar[] v = new IntVar[]{a,b,c,d,e,f,g,h,i,x,y,z,bc,aa,de,ff,gh,ii,yy,xz };
         
         // search for a solution and print results
         Search<IntVar> search = new DepthFirstSearch<IntVar>();
@@ -52,6 +78,16 @@ public class Chandelier {
         
         if (result) {
             System.out.println("Solution:");
+            System.out.println("a:" + a.value());
+            System.out.println("b:" + b.value());
+            System.out.println("c:" + c.value());
+            System.out.println("d:" + d.value());
+            System.out.println("e:" + e.value());
+            System.out.println("f:" + f.value());
+            System.out.println("g:" + g.value());
+            System.out.println("h:" + h.value());
+            System.out.println("i:" + i.value());
+            
         } else System.out.println("No solution");
     }
 }
