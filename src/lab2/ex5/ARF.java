d3e60a1b2697fd5bc76bffcdbf564a5e7253e7bc
ada10lgu <ada10lgu@student.lth.se>
2014-02-06 16:28:50 +0100

    Lab2, Ex5: Done! Waiting for approval

--- src/lab2/ex5/ARF.java
+++ src/lab2/ex5/ARF.java
@@ -2,6 +2,7 @@ package lab2.ex5;
 
 import java.util.ArrayList;
 
+import org.jacop.constraints.Diff2;
 import org.jacop.constraints.Linear;
 import org.jacop.core.IntVar;
 import org.jacop.core.Store;
@@ -15,8 +16,8 @@ import org.jacop.search.SmallestDomain;
 
 public class ARF {
 
-	private static final int NBR_ADD = 2;
-	private static final int NBR_MUL = 2;
+	private static final int NBR_ADD = 13;
+	private static final int NBR_MUL = 1;
 	private static final int TIME_ADD = 1;
 	private static final int TIME_MUL = 2;
 
@@ -27,78 +28,135 @@ public class ARF {
 
 	public ARF() {
 		Store store = new Store();
-		
+
 		ArrayList<IntVar> all = new ArrayList<IntVar>();
+
+		Add a = new Add(store, all, "A");
+		Add b = new Add(store, all, "B");
+		Mul c = new Mul(store, all, "C", a, b);
+		Add d = new Add(store, all, "D", c, a);
+
+		store.impose(new Diff2(Add.getRectangles()));
+		store.impose(new Diff2(Mul.getRectangles()));
 		
-		Add a = new Add(store,all);
-		Add b = new Add(store,all);
-		Mul c = new Mul(store,all,a,b);
-		Add d = new Add(store,all,c,a);
 		
-		
-		
-		Search<IntVar> label = new DepthFirstSearch<IntVar>(); 
-		SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(all.toArray(new IntVar[0]), 
-							    new SmallestDomain<IntVar>(), 
-							    new IndomainMin<IntVar>()); 
+		Search<IntVar> label = new DepthFirstSearch<IntVar>();
+		SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(
+				all.toArray(new IntVar[0]), new SmallestDomain<IntVar>(),
+				new IndomainMin<IntVar>());
 		label.setSolutionListener(new PrintOutListener<IntVar>());
 		boolean Result = label.labeling(store, select, d.ends);
-			
+
 		if (Result) {
-		    System.out.println("\n*** Yes");
-		    System.out.println("Solution : "+ java.util.Arrays.asList(all));
-		}
-		else System.out.println("\n*** No");
-	    
+			System.out.println("\n*** Yes");
+			System.out.println("Solution : " + java.util.Arrays.asList(all));
+		} else
+			System.out.println("\n*** No");
+
 	}
 
-
 	private static abstract class Operand {
-		protected Operand[] parents;
 
 		protected Store store;
+		
 		protected IntVar starts;
 		protected IntVar ends;
-
-		public Operand(Store store, ArrayList<IntVar> all, Operand... parents) {
+		
+		public Operand(Store store, ArrayList<IntVar> all, String name,
+				Operand... parents) {
 			this.store = store;
-			this.parents = parents;
-			starts = new IntVar(store, 0, 1000);
-			ends = new IntVar(store, 0, 1000);
+			starts = new IntVar(store, name + "-start", 0, 1000);
+			ends = new IntVar(store, name + "-end", 0, 1000);
 			all.add(starts);
 			all.add(ends);
+			addRectangle();
 			store.impose(new Linear(store, new IntVar[] { starts, ends },
 					new int[] { -1, 1 }, "=", cost()));
-			for (Operand p : parents) {
+			for (Operand p : parents)
 				store.impose(new Linear(store, new IntVar[] { starts, p.ends },
 						new int[] { 1, -1 }, ">=", 0));
-			}
+
 		}
+
 		protected abstract int cost();
+
+		protected abstract void addRectangle();
 	}
 
 	private static class Add extends Operand {
 
-		public Add(Store store, ArrayList<IntVar> all, Operand... parents) {
-			super(store,all, parents);
+		private static Rectangles rectangles;
+
+		public Add(Store store, ArrayList<IntVar> all, String name,
+				Operand... parents) {
+			super(store, all, name, parents);
 		}
 
 		@Override
 		protected int cost() {
 			return TIME_ADD;
 		}
+
+		@Override
+		protected void addRectangle() {
+			if (rectangles == null)
+				rectangles = new Rectangles(store);
+			rectangles.addRectangle(starts, TIME_ADD, NBR_ADD);
+		}
+
+		protected static  IntVar[][] getRectangles() {
+			return rectangles.getRectangles();
+		}
+
 	}
 
 	private static class Mul extends Operand {
 
-		public Mul(Store store, ArrayList<IntVar> all, Operand... parents) {
-			super(store, all, parents);
+		private static Rectangles rectangles;
+
+		public Mul(Store store, ArrayList<IntVar> all, String name,
+				Operand... parents) {
+			super(store, all, name, parents);
 		}
 
 		@Override
 		protected int cost() {
 			return TIME_MUL;
 		}
+
+		@Override
+		protected void addRectangle() {
+			if (rectangles == null)
+				rectangles = new Rectangles(store);
+			rectangles.addRectangle(starts, TIME_MUL, NBR_MUL);
+		}
+
+		protected static IntVar[][] getRectangles() {
+			return rectangles.getRectangles();
+		}
+
 	}
 
+	private static class Rectangles {
+		private Store store;
+		private ArrayList<IntVar[]> rectangles;
+		
+		
+		public Rectangles(Store store) {
+			this.store = store;
+			rectangles = new ArrayList<IntVar[]>();
+		}
+
+		public void addRectangle(IntVar starts, int length, int recources) {
+			IntVar y = new IntVar(store,0,recources-1);
+			IntVar dy = new IntVar(store,1,1);
+			IntVar dx = new IntVar(store,length,length);
+			IntVar[] rect = new IntVar[]{starts,y,dx,dy};
+			rectangles.add(rect);
+		}
+		
+		public IntVar[][] getRectangles() {
+			return rectangles.toArray(new IntVar[0][0]);
+		}
+	}
 }
