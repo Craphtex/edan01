70c4b884f70fb9d52fad380c7acea2fedc7d1853
ada10lgu <ada10lgu@student.lth.se>
2014-02-06 12:17:24 +0100

    Lab2, Ex1: Calculates workers not depending on cost

--- src/lab2/ex1/Scedule.java
+++ src/lab2/ex1/Scedule.java
@@ -1,9 +1,17 @@
 package lab2.ex1;
 
 import org.jacop.constraints.Constraint;
+import org.jacop.constraints.Linear;
 import org.jacop.constraints.Sum;
 import org.jacop.core.IntVar;
 import org.jacop.core.Store;
+import org.jacop.search.DepthFirstSearch;
+import org.jacop.search.IndomainMin;
+import org.jacop.search.PrintOutListener;
+import org.jacop.search.Search;
+import org.jacop.search.SelectChoicePoint;
+import org.jacop.search.SimpleSelect;
+import org.jacop.search.SmallestDomain;
 
 public class Scedule {
 	private Store store;
@@ -37,18 +45,77 @@ public class Scedule {
 		IntVar sumMoFull = new IntVar(store, 0, 100);
 		store.impose(new Sum(new IntVar[] { Mo, Su, Sa, Fr, Th }, sumMoFull));
 
+		IntVar[] fullSum = new IntVar[7];
+		for (int i = 0; i < week.length; i++) {
+			fullSum[i] = worker(i, 5);
+		}
+
+		IntVar[] partSum = new IntVar[7];
+		for (int i = 0; i < week.length; i++) {
+			partSum[i] = worker(i, 2);
+		}
+
+		int[] workers = new int[] { 17, 13, 15, 19, 14, 16, 11 };
+		for (int i = 0; i < week.length; i++) {
+			IntVar sum = new IntVar(store, workers[i], 100);
+			store.impose(new Sum(new IntVar[] { fullSum[i], partSum[i] }, sum));
+		}
+
 		IntVar cost = new IntVar(store, "cost", 0, 100);
 		store.impose(new Sum(week, cost));
 
+		System.out.println("Number of variables: " + store.size()
+				+ "\nNumber of constraints: " + store.numberConstraints());
+
+		Search<IntVar> label = new DepthFirstSearch<IntVar>();
+		SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(week,
+				new SmallestDomain<IntVar>(), new IndomainMin<IntVar>());
+		label.setSolutionListener(new PrintOutListener<IntVar>());
+		boolean Result = label.labeling(store, select, cost);
+
+		if (Result) {
+			System.out.println("\n*** Yes");
+			System.out.println("Solution : " + java.util.Arrays.asList(week));
+		} else
+			System.out.println("\n*** No");
+
 	}
 
-	private Constraint worker(int start, int length) {
+	private IntVar worker(int start, int length) {
 		IntVar[] temp = new IntVar[length];
 		for (int i = 0; i < length; i++) {
-			temp[i] = week[(start - i) % week.length];
+			temp[i] = week[(start - i + week.length) % week.length];
 		}
-		IntVar sum = new IntVar(store, 0, 100);
-		return new Sum(temp, sum);
+		String name = length == 5 ? "Full" : "Part";
+		String day = "";
+		switch (start) {
+		case 0:
+			day = "Monday";
+			break;
+		case 1:
+			day = "Tuesday";
+			break;
+		case 2:
+			day = "Wednesday";
+			break;
+		case 3:
+			day = "Thursday";
+			break;
+		case 4:
+			day = "Friday";
+			break;
+		case 5:
+			day = "Saturday";
+			break;
+		case 6:
+			day = "Sunday";
+			break;
+			
+		
+		}
+		IntVar sum = new IntVar(store,name+"-"+day, 0, 100);
+		store.impose(new Sum(temp, sum));
+		return sum;
 
 	}
 }
