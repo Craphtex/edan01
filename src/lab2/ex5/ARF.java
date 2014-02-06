1e4911a8c7d3587eb87f0d40c9b7d2a1a7c0e05f
ada10lgu <ada10lgu@student.lth.se>
2014-02-06 15:37:10 +0100

    Lab2, Ex5: Now solved the problem without recourse constraint

--- src/lab2/ex5/ARF.java
+++ src/lab2/ex5/ARF.java
@@ -1,65 +1,86 @@
 package lab2.ex5;
 
+import java.util.ArrayList;
+
 import org.jacop.constraints.Linear;
 import org.jacop.core.IntVar;
 import org.jacop.core.Store;
+import org.jacop.search.DepthFirstSearch;
+import org.jacop.search.IndomainMin;
+import org.jacop.search.PrintOutListener;
+import org.jacop.search.Search;
+import org.jacop.search.SelectChoicePoint;
+import org.jacop.search.SimpleSelect;
+import org.jacop.search.SmallestDomain;
 
 public class ARF {
-	
+
 	private static final int NBR_ADD = 2;
 	private static final int NBR_MUL = 2;
 	private static final int TIME_ADD = 1;
 	private static final int TIME_MUL = 2;
-	
-	
-	
+
 	public static void main(String[] args) {
 		new ARF();
-		
+
 	}
-	
-	
+
 	public ARF() {
 		Store store = new Store();
 		
+		ArrayList<IntVar> all = new ArrayList<IntVar>();
 		
-		Add a = new Add(store);
-		Add b = new Add(store);
-		Mul c = new Mul(store,a,b);
-		Add d = new Add(store,c,a);
+		Add a = new Add(store,all);
+		Add b = new Add(store,all);
+		Mul c = new Mul(store,all,a,b);
+		Add d = new Add(store,all,c,a);
 		
 		
 		
-		
+		Search<IntVar> label = new DepthFirstSearch<IntVar>(); 
+		SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(all.toArray(new IntVar[0]), 
+							    new SmallestDomain<IntVar>(), 
+							    new IndomainMin<IntVar>()); 
+		label.setSolutionListener(new PrintOutListener<IntVar>());
+		boolean Result = label.labeling(store, select, d.ends);
+			
+		if (Result) {
+		    System.out.println("\n*** Yes");
+		    System.out.println("Solution : "+ java.util.Arrays.asList(all));
+		}
+		else System.out.println("\n*** No");
+	    
 	}
-	
-	
-	
-	private static abstract class Operand{
+
+
+	private static abstract class Operand {
 		protected Operand[] parents;
-		
+
 		protected Store store;
 		protected IntVar starts;
 		protected IntVar ends;
-		
-		public Operand(Store store, Operand... parents) {
+
+		public Operand(Store store, ArrayList<IntVar> all, Operand... parents) {
 			this.store = store;
 			this.parents = parents;
-			starts = new IntVar(store,0,1000);
-			ends = new IntVar(store,0,1000);
-			store.impose(new Linear(store, new IntVar[]{starts,ends}, new int[]{-1,1}, "=", cost()));
+			starts = new IntVar(store, 0, 1000);
+			ends = new IntVar(store, 0, 1000);
+			all.add(starts);
+			all.add(ends);
+			store.impose(new Linear(store, new IntVar[] { starts, ends },
+					new int[] { -1, 1 }, "=", cost()));
 			for (Operand p : parents) {
-				store.impose(new Linear(store, new IntVar[]{starts,p.ends}, new int[]{1,-1}, ">=", 0));
+				store.impose(new Linear(store, new IntVar[] { starts, p.ends },
+						new int[] { 1, -1 }, ">=", 0));
 			}
 		}
-		
 		protected abstract int cost();
 	}
-	
+
 	private static class Add extends Operand {
 
-		public Add(Store store, Operand... parents) {
-			super(store, parents);
+		public Add(Store store, ArrayList<IntVar> all, Operand... parents) {
+			super(store,all, parents);
 		}
 
 		@Override
@@ -67,11 +88,11 @@ public class ARF {
 			return TIME_ADD;
 		}
 	}
-	
+
 	private static class Mul extends Operand {
 
-		public Mul(Store store, Operand... parents) {
-			super(store, parents);
+		public Mul(Store store, ArrayList<IntVar> all, Operand... parents) {
+			super(store, all, parents);
 		}
 
 		@Override
@@ -79,6 +100,5 @@ public class ARF {
 			return TIME_MUL;
 		}
 	}
-	
-	
+
 }
